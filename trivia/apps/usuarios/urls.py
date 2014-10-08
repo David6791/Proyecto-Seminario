476ce416a83ba2acb216890a56728bf86054e3dd
joaquin <joaquin1837@gmail.com>
2014-10-07 22:28:19 -0700

    Segudno commit

--- trivia/apps/usuarios/urls.py
+++ trivia/apps/usuarios/urls.py
@@ -6,6 +6,5 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'chatgrafico.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', pagina_principal),
-	
+    url(r'^registro/$',registro_usuarios),	
 )
--- trivia/apps/usuarios/views.py
+++ trivia/apps/usuarios/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,logout,authenticate
+import pdb
 # Create your views here.
+def registro_usuarios(request):
+	if(request.method=="POST"):
+		formuser=UserCreationForm(request.POST)
+		if(formuser.is_valid()):
+			formuser.save()
+			return HttpResponseRedirect("/inicio/registro/")
+	formuser=UserCreationForm()
+	return render_to_response("usuario/registro.html", {"formuser":formuser}, RequestContext(request))
